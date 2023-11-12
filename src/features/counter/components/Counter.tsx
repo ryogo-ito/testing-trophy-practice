f876db781921e2b27168c6ff6ba59b8994c2f742
ryogo-ito <106786725+ryogo-ito@users.noreply.github.com>
2023-11-12 15:28:53 +0900

    counterのstyling

--- src/features/counter/components/Counter.tsx
+++ src/features/counter/components/Counter.tsx
@@ -2,16 +2,21 @@ import { useState } from 'react';
 
 import reactLogo from '../../../assets/react.svg';
 
+import styles from './Counter.module.css';
+
 export function Counter() {
+  // TODO リフトアップするか考える
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <a href="https://react.dev" target="_blank">
+    <div className={styles.module}>
+      <a href="https://react.dev" target="_blank" className={styles.link}>
         <img src={reactLogo} className="logo react" alt="React logo" />
       </a>
       <h1>Counter Feature</h1>
-      <button onClick={() => setCount((prevCount) => prevCount + 1)}>count is {count}</button>
-    </>
+      <button onClick={() => setCount((prevCount) => prevCount + 1)} className={styles.button}>
+        count is {count}
+      </button>
+    </div>
   );
 }
